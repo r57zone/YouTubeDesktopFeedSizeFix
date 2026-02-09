c3c1b51c252f753cc2cfbbf1cc651a8941968553
r57zone <r57zone@gmail.com>
2026-02-09 12:18:51 +0400

    Initial

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# YouTube-Desktop-Feed-Size-Fix
-Userscript to fix YouTube video preview sizes in the feed / Userscript для исправления размеров превью ютуб видео в ленте
+# YouTube Desktop Feed Size Fix
+En: Userscript to fix YouTube video preview sizes in the feed.
+
+Ru: Userscript для исправления размеров превью ютуб видео в ленте.
+
+<img width="600" height="145" src="https://github.com/user-attachments/assets/2c90c925-0e64-4467-bf72-a0c18076ed1b" />
+
+
---
+++ youtube.js
@@ -0,0 +1,75 @@
+// ==UserScript==
+// @name         YouTubeDesktopFeedSizeFix
+// @namespace    https://github.com/r57zone/YouTubeDesktopFeedSizeFix
+// @version      1.0
+// @match        https://www.youtube.com/*
+// @run-at       document-start
+// ==/UserScript==
+
+(function() {
+    'use strict';
+  
+    function applyExtraCSS() {
+      let style = document.getElementById('yt-extra-style');
+      if (!style) {
+          style = document.createElement('style');
+          style.id = 'yt-extra-style';
+          document.head.appendChild(style);
+      }
+
+      style.textContent =
+          '#content.ytd-rich-section-renderer {' +
+          'width: 50% !important;' +
+          '}';
+  	}
+
+
+    function fixGrid() {
+        const grids = document.querySelectorAll('ytd-rich-grid-renderer');
+
+        grids.forEach(function(grid) {
+            grid.setAttribute('elements-per-row', '6');
+            grid.style.setProperty('--ytd-rich-grid-item-max-width', '320px', 'important');
+            grid.style.setProperty('--ytd-rich-grid-items-per-row', '6', 'important');
+        });
+    }
+  
+  	function fixAll() {
+        fixGrid();
+        applyExtraCSS();
+    }
+
+    // ждём пока появится grid
+    function waitForGrid() {
+        if (document.querySelector('ytd-rich-grid-renderer'))
+            fixAll();
+        else
+            setTimeout(waitForGrid, 300);
+    }
+
+    // после загрузки
+    window.addEventListener('load', function() {
+        setTimeout(waitForGrid, 800);
+    });
+
+    // при SPA-навигации
+    document.addEventListener('yt-navigate-finish', function() {
+        setTimeout(waitForGrid, 800);
+    });
+
+    // при скролле
+    window.addEventListener('scroll', function() {
+        fixAll();
+    });
+  
+  
+ 	//let resizeTimer = null;
+
+  /*window.addEventListener('resize', function() {
+      clearTimeout(resizeTimer);
+      resizeTimer = setTimeout(function() {
+          fixAll();
+      }, 300);
+  });*/
+
+})();
